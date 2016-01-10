eece08329b5cddd2ca013f3a93f58a63e102572c
pnavarez <greywolf792002@gmail.com>
2016-01-10 20:41:24 +0800

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        Model::unguard();
+        
+        $this->call(ProvincesTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+
+        Model::reguard();
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,5 +12,6 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('users')->insert(['name'=>'Philip Radin Navarez', 'email'=>'greywolf792002@gmail.com', 'password'=>'$2y$10$r914VrMdsrar86vGd0OcXe/IOQHtjwtxINCb3BC5c0nd2meSfe83m']);
     }
 }
