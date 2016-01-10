dfdf0bd9575adf9a9f056a44fa895dc6f26a1cbb
pnavarez <greywolf792002@gmail.com>
2016-01-11 00:46:45 +0800

    views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Prenda</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-        <link href="/css/app.css" rel="stylesheet">
+        <link href="css/app.css" rel="stylesheet">
     </head>
     <body id="app-layout">
         @include('includes.navigation')
@@ -14,7 +14,7 @@
             @yield('content')
             @include('includes.footer')
         </div>
-        <script src="/js/vendor.js"></script>
-        <script src="/js/app.js"></script>
+        <script src="js/vendor.js"></script>
+        <script src="js/app.js"></script>
     </body>
 </html>
