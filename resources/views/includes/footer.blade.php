732950d5086c8b06dfdeb6d39a644d4a200f40d3
pnavarez <greywolf792002@gmail.com>
2016-01-11 00:43:23 +0800

    views

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="container marketing">
+    <footer>
+        <hr>
+        <span class="pull-left">Prenda</span>
+        <span class="pull-right">NavarezCorp</span>
+    </footer>
+</div>
